3aaa4e423860516c7c6d272036aa838bfcf36fb5
Taron <saribekyantaron1998@gmail.com>
2022-06-29 16:50:43 +0400

    Initial Commit

---
+++ HotelRegistrations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotelRegistrations
+//
+//  Created by Taron on 29.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
